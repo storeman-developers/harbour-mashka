089d85943aa559d7162a2979acbbbd4270aac58e
A <hucul@o2.pl>
2018-03-10 18:34:42 +0100

    Create harbour-mashka-pl.ts

---
+++ translations/harbour-mashka-pl.ts
@@ -0,0 +1,258 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl">
+<context>
+    <name></name>
+    <message id="mashka-rescan">
+        <location filename="../qml/components/RescanMenuEntry.qml" line="8"/>
+        <location filename="../qml/cover/CoverPage.qml" line="17"/>
+        <source>Rescan</source>
+        <translation>Skanuj ponownie</translation>
+    </message>
+    <message id="mashka-working">
+        <location filename="../qml/cover/CoverPage.qml" line="12"/>
+        <source>Working...</source>
+        <translation>Działa...</translation>
+    </message>
+    <message id="mashka-cover-could-be-cleared">
+        <location filename="../qml/cover/CoverPage.qml" line="15"/>
+        <source>%1 could be cleared</source>
+        <translation>%1 może zostać wyczyszczone</translation>
+    </message>
+    <message id="mashka-bytes" numerus="yes">
+        <location filename="../qml/harbour-mashka.qml" line="18"/>
+        <source>%n byte(s)</source>
+        <translation>
+            <numerusform>%n bajt</numerusform>
+            <numerusform>%n bajty</numerusform>
+            <numerusform>%n bajtów</numerusform>
+        </translation>
+    </message>
+    <message id="mashka-kb" numerus="yes">
+        <location filename="../qml/harbour-mashka.qml" line="21"/>
+        <source>%n KB</source>
+        <translation>
+            <numerusform>%n KB</numerusform>
+            <numerusform>%n KB</numerusform>
+            <numerusform>%n KB</numerusform>
+        </translation>
+    </message>
+    <message id="mashka-mb" numerus="yes">
+        <location filename="../qml/harbour-mashka.qml" line="24"/>
+        <source>%n MB</source>
+        <translation>
+            <numerusform>%n MB</numerusform>
+            <numerusform>%n MB</numerusform>
+            <numerusform>%n MB</numerusform>
+        </translation>
+    </message>
+    <message id="mashka-gb" numerus="yes">
+        <location filename="../qml/harbour-mashka.qml" line="27"/>
+        <source>%n GB</source>
+        <translation>
+            <numerusform>%n GB</numerusform>
+            <numerusform>%n GB</numerusform>
+            <numerusform>%n GB</numerusform>
+        </translation>
+    </message>
+    <message id="mashka-notification-cleared">
+        <location filename="../qml/harbour-mashka.qml" line="70"/>
+        <source>Cleared %1</source>
+        <translation>Wyczyszczone %1</translation>
+    </message>
+    <message id="mashka-notification-error-title">
+        <location filename="../qml/harbour-mashka.qml" line="76"/>
+        <source>An error occured!</source>
+        <translation>Wystąpił błąd!</translation>
+    </message>
+    <message id="mashka-notification-error-body">
+        <location filename="../qml/harbour-mashka.qml" line="78"/>
+        <source>Error removing &amp;quot;%1&amp;quot;.</source>
+        <translation>Błąd usuwania &amp;quot;%1&amp;quot;.</translation>
+    </message>
+    <message id="mashka-banner">
+        <location filename="../qml/pages/BannerPage.qml" line="25"/>
+        <source>&lt;h3&gt;Ahoy sailor!&lt;/h3&gt;&lt;br /&gt;&lt;p&gt;You&apos;re using the application for some time and have already cleared more than 50&amp;nbsp;MB of unused data.&lt;/p&gt;&lt;br /&gt;&lt;p&gt;Please consider to make a small donation to support the developer ;)&lt;/p&gt;</source>
+        <translation>&lt;h3&gt;Ahoj marynarzu!&lt;/h3&gt;&lt;br /&gt;&lt;p&gt;Używasz aplikacji już od jakiegoś czasu i wyczyściłeś już ponad 50&amp;nbsp;MB nieużywanych danych.&lt;/p&gt;&lt;br /&gt;&lt;p&gt;Rozważ małą dotację w celu wsparcia programisty ;)&lt;/p&gt;</translation>
+    </message>
+    <message id="mashka-not-show">
+        <location filename="../qml/pages/BannerPage.qml" line="50"/>
+        <source>Don&apos;t show again</source>
+        <translation>Nie pokazuj ponownie</translation>
+    </message>
+    <message id="mashka-found">
+        <location filename="../qml/pages/EntriesPage.qml" line="32"/>
+        <location filename="../qml/pages/MainPage.qml" line="86"/>
+        <source>Found</source>
+        <translation>Znaleziono</translation>
+    </message>
+    <message id="mashka-installed">
+        <location filename="../qml/pages/EntriesPage.qml" line="42"/>
+        <source>Installed</source>
+        <translation>Zainstalowane</translation>
+    </message>
+    <message id="mashka-uninstalled">
+        <location filename="../qml/pages/EntriesPage.qml" line="44"/>
+        <source>Uninstalled</source>
+        <translation>Odinstalowane</translation>
+    </message>
+    <message id="mashka-config">
+        <location filename="../qml/components/EntryDelegate.qml" line="92"/>
+        <source>Configuration</source>
+        <translation>Konfiguracja</translation>
+    </message>
+    <message id="mashka-cache">
+        <location filename="../qml/components/EntryDelegate.qml" line="98"/>
+        <source>Cache</source>
+        <translation>Pamięć podręczna</translation>
+    </message>
+    <message id="mashka-delete-config">
+        <location filename="../qml/components/EntryDelegate.qml" line="32"/>
+        <source>Delete configuration</source>
+        <translation>Usuń konfigurację</translation>
+    </message>
+    <message id="mashka-delete-all">
+        <location filename="../qml/components/EntryDelegate.qml" line="21"/>
+        <source>Delete all data</source>
+        <translation>Usuń wszystkie dane</translation>
+    </message>
+    <message id="mashka-deleting-alldata">
+        <location filename="../qml/components/EntryDelegate.qml" line="23"/>
+        <source>Deleting all data</source>
+        <translation>Usuwanie wszystkich danych</translation>
+    </message>
+    <message id="mashka-deleting-config">
+        <location filename="../qml/components/EntryDelegate.qml" line="34"/>
+        <source>Deleting configuration</source>
+        <translation>Usuwanie konfiguracji</translation>
+    </message>
+    <message id="mashka-delete-cache">
+        <location filename="../qml/components/EntryDelegate.qml" line="43"/>
+        <source>Delete cache</source>
+        <translation>Usun pamięć podręczną</translation>
+    </message>
+    <message id="mashka-deleting-cache">
+        <location filename="../qml/components/EntryDelegate.qml" line="45"/>
+        <source>Deleting cache</source>
+        <translation>Usuwanie pamięci podręcznej</translation>
+    </message>
+    <message id="mashka-delete-localdata">
+        <location filename="../qml/components/EntryDelegate.qml" line="54"/>
+        <source>Delete local data</source>
+        <translation>Usuń dane lokalne</translation>
+    </message>
+    <message id="mashka-deleting-localdata">
+        <location filename="../qml/components/EntryDelegate.qml" line="56"/>
+        <source>Deleting local data</source>
+        <translation>Usuwanie danych lokalnych</translation>
+    </message>
+    <message id="mashka-localdata">
+        <location filename="../qml/components/EntryDelegate.qml" line="104"/>
+        <source>Local data</source>
+        <translation>Dane lokalne</translation>
+    </message>
+    <message id="mashka-of-data">
+        <location filename="../qml/pages/EntriesPage.qml" line="33"/>
+        <location filename="../qml/pages/MainPage.qml" line="88"/>
+        <location filename="../qml/pages/MainPage.qml" line="97"/>
+        <location filename="../qml/pages/MainPage.qml" line="106"/>
+        <source>%1 of data</source>
+        <translation>%1 danych</translation>
+    </message>
+    <message id="mashka-of-apps" numerus="yes">
+        <location filename="../qml/pages/EntriesPage.qml" line="34"/>
+        <location filename="../qml/pages/MainPage.qml" line="90"/>
+        <source>of %n application(s)</source>
+        <translation>
+            <numerusform>of %n aplikacji</numerusform>
+            <numerusform>of %n aplikacji</numerusform>
+            <numerusform>of %n aplikacji</numerusform>
+        </translation>
+    </message>
+    <message id="mashka-hint-openentriespage">
+        <location filename="../qml/pages/MainPage.qml" line="27"/>
+        <source>Swipe to see details</source>
+        <translation>Przesuń aby zobaczyć szczegóły</translation>
+    </message>
+    <message id="mashka-unused">
+        <location filename="../qml/pages/MainPage.qml" line="96"/>
+        <source>Unused</source>
+        <translation>Nieużywane</translation>
+    </message>
+    <message id="mashka-of-removed-apps" numerus="yes">
+        <location filename="../qml/pages/MainPage.qml" line="99"/>
+        <source>of %n removed application(s)</source>
+        <translation>
+            <numerusform>of %n usuniętej aplikacji</numerusform>
+            <numerusform>of %n usuniętych aplikacji</numerusform>
+        </translation>
+    </message>
+    <message id="mashka-total-cleared">
+        <location filename="../qml/pages/MainPage.qml" line="105"/>
+        <source>Total cleared</source>
+        <translation>Całkowicie wyczyszczone</translation>
+    </message>
+    <message id="mashka-clear">
+        <location filename="../qml/pages/MainPage.qml" line="126"/>
+        <source>Clear unused data</source>
+        <translation>Wyczyść nieużywane dane</translation>
+    </message>
+    <message id="mashka-placeholder">
+        <location filename="../qml/pages/MainPage.qml" line="136"/>
+        <source>No unused data was found</source>
+        <translation>Nie znaleziono nieużywanych danych</translation>
+    </message>
+    <message id="mashka-empty">
+        <location filename="../qml/components/SizeLabel.qml" line="13"/>
+        <source>empty</source>
+        <translation>pusty</translation>
+    </message>
+    <message id="mashka-confirmation-warning">
+        <location filename="../qml/pages/ConfirmationDialog.qml" line="45"/>
+        <source>&lt;h3&gt;Warning!&lt;/h3&gt;&lt;br /&gt;&lt;p&gt;This action can damage your device. You accept it solely at your own risk!&lt;/p&gt;</source>
+        <translation>&lt;h3&gt;Ostrzeżenie!&lt;/h3&gt;&lt;br /&gt;&lt;p&gt;Ta czynność może uszkodzić urządzenie. Robisz to na własną odpoweidzialność!&lt;/p&gt;</translation>
+    </message>
+    <message id="mashka-understand1">
+        <location filename="../qml/pages/ConfirmationDialog.qml" line="51"/>
+        <source>I understand the risks</source>
+        <translation>Rozumiem ryzyko</translation>
+    </message>
+    <message id="mashka-understand2">
+        <location filename="../qml/pages/ConfirmationDialog.qml" line="59"/>
+        <source>I completely understand the risks</source>
+        <translation>Całkowicie rozumiem ryzyko</translation>
+    </message>
+    <message id="mashka-about">
+        <location filename="../qml/pages/AboutPage.qml" line="20"/>
+        <location filename="../qml/pages/MainPage.qml" line="56"/>
+        <source>About Mashka</source>
+        <translation>O Mashka</translation>
+    </message>
+    <message id="mashka-app-description">
+        <location filename="../qml/pages/AboutPage.qml" line="55"/>
+        <source>&lt;p&gt;Mashka is the slang name of the mop for deck washing used by Russian sailors.&lt;/p&gt;&lt;p&gt;Mashka searches for and cleares unused data leaved after removed Sailfish&amp;nbsp;OS applications.&lt;/p&gt;</source>
+        <translation>&lt;p&gt;Mashka jest potocznym określeniem mopa używanego przez rosyjskich żeglarzy.&lt;/p&gt;&lt;p&gt;Mashka wyszukuje i czyści nieużywane dane, zostawione po odinstalowanych aplikacjach.&lt;/p&gt;</translation>
+    </message>
+    <message id="mashka-app-licensing">
+        <location filename="../qml/pages/AboutPage.qml" line="58"/>
+        <source>&lt;p&gt;This is an open source software which is distributed under the terms of the&lt;br /&gt;&lt;a href=&apos;%1&apos;&gt;MIT License&lt;/a&gt;.&lt;/p&gt;</source>
+        <translation>&lt;p&gt;To jest otwartoźródłowe oprogramowanie dystrybuowane na licencji&lt;br /&gt;&lt;a href=&apos;%1&apos;&gt;MIT License&lt;/a&gt;.&lt;/p&gt;</translation>
+    </message>
+    <message id="mashka-development">
+        <location filename="../qml/pages/AboutPage.qml" line="67"/>
+        <source>Development</source>
+        <translation>Rozwój</translation>
+    </message>
+    <message id="mashka-translations">
+        <location filename="../qml/pages/AboutPage.qml" line="73"/>
+        <source>Translations</source>
+        <translation>Tłumaczenia</translation>
+    </message>
+    <message id="mashka-donate-with">
+        <location filename="../qml/pages/AboutPage.qml" line="79"/>
+        <location filename="../qml/pages/AboutPage.qml" line="84"/>
+        <source>Donate with %1</source>
+        <translation>Wpomóż z %1</translation>
+    </message>
+</context>
+</TS>
