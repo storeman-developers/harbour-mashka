fb6ae68c51b4387c403b40533ae1bb635146e455
Petr Tsymbarovich <petr@tsymbarovich.ru>
2021-01-16 20:24:47 +0300

    Optimize string constants

--- src/mashka.cpp
+++ src/mashka.cpp
@@ -3,28 +3,40 @@
 #include <QSettings>
 #include <QMetaEnum>
 
+static const QString LAUNCHES_KEY{QStringLiteral("Launches")};
+static const QString BANNERSEEN_KEY{QStringLiteral("BannerSeen")};
+static const QString TOTALDELETED_KEY{QStringLiteral("TotalDeleted")};
+static const QString ADVANCEDOPTIONS_KEY{QStringLiteral("AdvancedOptionsEnabled")};
+static const QString PROCESSCONFIG_KEY{QStringLiteral("ProcessConfigEnabled")};
+static const QString DELETEALLDATA_KEY{QStringLiteral("DeleteAllDataAllowed")};
+
+QString hint_key(Mashka::Hint hint) {
+    auto me   = QMetaEnum::fromType<Mashka::Hint>();
+    auto name = me.valueToKey(hint);
+    Q_ASSERT(name);
+    return QStringLiteral("Hints/").append(name);
+}
 
 Mashka::Mashka(QObject *parent)
     : QObject{parent}
     , m_settings{new QSettings{this}}
 {
-    QString key(QStringLiteral("Launches"));
-    auto current = m_settings->value(key, 0).toUInt();
+    auto current = m_settings->value(LAUNCHES_KEY, 0).toUInt();
     if (current < 10)
     {
-        m_settings->setValue(key, current + 1);
+        m_settings->setValue(LAUNCHES_KEY, current + 1);
     }
 }
 
 bool Mashka::showBanner() const
 {
-    if (m_settings->value(QStringLiteral("BannerSeen"), false).toBool())
+    if (m_settings->value(BANNERSEEN_KEY, false).toBool())
     {
         return false;
     }
 
     auto deleted  = this->totalDeletedData();
-    auto launches = m_settings->value(QStringLiteral("Launches"), 0).toUInt();
+    auto launches = m_settings->value(LAUNCHES_KEY, 0).toUInt();
 
     // 52428800 == 50 MB
     return deleted > 52428800 && launches >= 10;
@@ -32,56 +44,48 @@ bool Mashka::showBanner() const
 
 void Mashka::setBannerShowed()
 {
-    m_settings->setValue(QStringLiteral("BannerSeen"), true);
+    m_settings->setValue(BANNERSEEN_KEY, true);
 }
 
 bool Mashka::showHint(const Hint &hint) const
 {
-    auto me = QMetaEnum::fromType<Hint>();
-    auto name = me.valueToKey(hint);
-    Q_ASSERT(name);
-    return !m_settings->value(QStringLiteral("Hints/").append(name), false).toBool();
+    return !m_settings->value(hint_key(hint), false).toBool();
 }
 
 void Mashka::setHintShowed(const Hint &hint)
 {
-    auto me = QMetaEnum::fromType<Hint>();
-    auto name = me.valueToKey(hint);
-    Q_ASSERT(name);
-    m_settings->setValue(QStringLiteral("Hints/").append(name), true);
+    m_settings->setValue(hint_key(hint), true);
 }
 
 qint64 Mashka::totalDeletedData() const
 {
-    return m_settings->value(QStringLiteral("TotalDeleted"), 0).toLongLong();
+    return m_settings->value(TOTALDELETED_KEY, 0).toLongLong();
 }
 
 void Mashka::addDeletedData(const qint64 &size)
 {
-    QString key(QStringLiteral("TotalDeleted"));
-    auto current = m_settings->value(key, 0).toLongLong();
-    m_settings->setValue(key, current + size);
+    auto current = m_settings->value(TOTALDELETED_KEY, 0).toLongLong();
+    m_settings->setValue(TOTALDELETED_KEY, current + size);
     emit this->totalDeletedDataChanged();
 }
 
 void Mashka::resetDeletedData()
 {
     qDebug("Resetting TotalDeleted value");
-    m_settings->remove(QStringLiteral("TotalDeleted"));
+    m_settings->remove(TOTALDELETED_KEY);
     emit this->totalDeletedDataChanged();
 }
 
 bool Mashka::advancedOptionsEnabled() const
 {
-    return m_settings->value(QStringLiteral("AdvancedOptionsEnabled")).toBool();
+    return m_settings->value(ADVANCEDOPTIONS_KEY).toBool();
 }
 
 void Mashka::setAdvancedOptionsEnabled(bool value)
 {
-    QString key(QStringLiteral("AdvancedOptionsEnabled"));
-    if (m_settings->value(key).toBool() != value)
+    if (m_settings->value(ADVANCEDOPTIONS_KEY).toBool() != value)
     {
-        m_settings->setValue(key, value);
+        m_settings->setValue(ADVANCEDOPTIONS_KEY, value);
         emit this->advancedOptionsEnabledChanged();
         if (!value)
         {
@@ -93,30 +97,28 @@ void Mashka::setAdvancedOptionsEnabled(bool value)
 
 bool Mashka::processConfigEnabled() const
 {
-    return m_settings->value(QStringLiteral("ProcessConfigEnabled")).toBool();
+    return m_settings->value(PROCESSCONFIG_KEY).toBool();
 }
 
 void Mashka::setProcessConfigEnabled(bool value)
 {
-    QString key(QStringLiteral("ProcessConfigEnabled"));
-    if (m_settings->value(key).toBool() != value)
+    if (m_settings->value(PROCESSCONFIG_KEY).toBool() != value)
     {
-        m_settings->setValue(key, value);
+        m_settings->setValue(PROCESSCONFIG_KEY, value);
         emit this->processConfigEnabledChanged();
     }
 }
 
 bool Mashka::deleteAllDataAllowed() const
 {
-    return m_settings->value(QStringLiteral("DeleteAllDataAllowed")).toBool();
+    return m_settings->value(DELETEALLDATA_KEY).toBool();
 }
 
 void Mashka::setDeleteAllDataAllowed(bool value)
 {
-    QString key(QStringLiteral("DeleteAllDataAllowed"));
-    if (m_settings->value(key).toBool() != value)
+    if (m_settings->value(DELETEALLDATA_KEY).toBool() != value)
     {
-        m_settings->setValue(key, value);
+        m_settings->setValue(DELETEALLDATA_KEY, value);
         emit this->deleteAllDataAllowedChanged();
     }
 }
