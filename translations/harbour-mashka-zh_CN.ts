7a769c34d6c4c59177fbdd85ea374af6a387833a
Petr Tsymbarovich <petr@tsymbarovich.ru>
2021-01-18 21:08:54 +0300

    Update translations

--- translations/harbour-mashka-zh_CN.ts
+++ translations/harbour-mashka-zh_CN.ts
@@ -32,14 +32,12 @@
     <message id="mashka-notification-error-body">
         <location filename="../qml/harbour-mashka.qml" line="52"/>
         <source>Error deleting &amp;quot;%1&amp;quot;.</source>
-        <oldsource>Error removing &amp;quot;%1&amp;quot;.</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>移除错误 &amp;quot;%1&amp;quot;。</translation>
     </message>
     <message id="mashka-banner">
         <location filename="../qml/pages/BannerPage.qml" line="24"/>
         <source>&lt;h3&gt;Ahoy sailor!&lt;/h3&gt;&lt;br /&gt;&lt;p&gt;You&apos;re using the application for some time and have already deleted more than %1 of unused data.&lt;/p&gt;&lt;br /&gt;&lt;p&gt;Please consider to make a small donation to support the developer ;)&lt;/p&gt;</source>
-        <oldsource>&lt;h3&gt;Ahoy sailor!&lt;/h3&gt;&lt;br /&gt;&lt;p&gt;You&apos;re using the application for some time and have already cleared more than %1 of unused data.&lt;/p&gt;&lt;br /&gt;&lt;p&gt;Please consider to make a small donation to support the developer ;)&lt;/p&gt;</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;你好，水手！&lt;/h3&gt;&lt;br /&gt;&lt;p&gt;你已经多次使用本软件，并且已清理超过 %1 未使用的数据。&lt;/p&gt;&lt;br /&gt;&lt;p&gt;请考虑做点小捐赠以支持开发者;)&lt;/p&gt;</translation>
     </message>
     <message id="mashka-not-show">
         <location filename="../qml/pages/BannerPage.qml" line="49"/>
@@ -198,8 +196,7 @@
     <message id="mashka-app-description">
         <location filename="../qml/pages/AboutPage.qml" line="54"/>
         <source>&lt;p&gt;Mashka is the slang name of the mop for deck washing used by Russian sailors.&lt;/p&gt;&lt;p&gt;Mashka searches for and deleted unused data leaved after removed Sailfish&amp;nbsp;OS applications.&lt;/p&gt;</source>
-        <oldsource>&lt;p&gt;Mashka is the slang name of the mop for deck washing used by Russian sailors.&lt;/p&gt;&lt;p&gt;Mashka searches for and cleares unused data leaved after removed Sailfish&amp;nbsp;OS applications.&lt;/p&gt;</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Mashka 是俄罗斯水手用于清理甲班的拖把的俚语。&lt;/p&gt;&lt;p&gt;Mashka 搜索并清理卸载旗鱼系统软件之后剩余的未使用的数据。&lt;/p&gt;</translation>
     </message>
     <message id="mashka-app-licensing">
         <location filename="../qml/pages/AboutPage.qml" line="57"/>
